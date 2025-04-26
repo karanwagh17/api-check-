57855d367c113292f9ba6c7b51053ec4e7eea4ec
karan wagh <167848916+karanwagh17@users.noreply.github.com>
2025-04-26 22:07:43 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose");
-
-const connetction = mongoose.connect("mongodb://127.0.0.1:27017/api")
-module.exports=connetction
--- backend/middleware/userLogger.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-module.exports = (req, res, next) => {
-  const { username, role } = req.user;
-  const logEntry = `${new Date().toISOString()} - ${username}:${role}\n`;
-  fs.appendFileSync(path.join(__dirname, "../log.txt"), logEntry);
-  next();
-};
